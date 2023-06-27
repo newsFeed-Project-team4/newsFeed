45ea1931ceedc2b33eb9ad0536dc7974b0490b5b
Sung min Park <somemen1234@naver.com>
2023-06-27 16:09:53 +0900

    sample.js 추가

--- libs/s3Client.js
+++ libs/s3Client.js
@@ -1,4 +1,4 @@
-const S3Client = require('aws-sdk/clients/s3');
+const { S3Client } = require('@aws-sdk/client-s3');
 const Region = 'ap-northeast-2';
 const s3Client = new S3Client({ region: Region });
 
